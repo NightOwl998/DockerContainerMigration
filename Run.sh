1ee02e642c21bf3b1e464bce2f61f4afb14197a9
NightOwl998 <64656757+NightOwl998@users.noreply.github.com>
2021-09-27 18:14:06 +0100

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 sudo  docker run -d --rm -p 172.16.1.16:8080:8080 -p 172.16.1.16:7777:7777/udp -p 172.16.1.16:7776:7776/udp -p 10.0.2.8:8080:8080 -p 10.0.2.8:7777:7777/udp -p 10.0.2.8:7776:7776/udp  -v /root/:/config --name lrm-node derekrs/lrm_node:Bleeding-Edge
